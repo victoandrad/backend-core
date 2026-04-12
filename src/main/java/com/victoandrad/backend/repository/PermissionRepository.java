1663204315325e87bc3af4edcd7e480ac76439a0
Victor Andrade <victor64gaming@gmail.com>
2026-04-12 18:56:19 -0300

    Repositories

---
+++ src/main/java/com/victoandrad/backend/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.victoandrad.backend.repository;
+
+import com.victoandrad.backend.domain.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
---
+++ src/main/java/com/victoandrad/backend/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.victoandrad.backend.repository;
+
+import com.victoandrad.backend.domain.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
